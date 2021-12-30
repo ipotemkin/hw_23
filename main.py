8abdabfd380443a165e63196fffe66f241040c11
ipotemkin <91094458+ipotemkin@users.noreply.github.com>
2021-12-30 18:37:56 +0600

    before adding flask-pydantic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_restx import reqparse, Api, Resource, inputs
-from utils import read_line_from_file, find_subsring, make_unique_lst, get_strings
+from utils import read_line_from_file, find_substring, make_unique_lst, get_strings
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -18,7 +18,7 @@ index_ns = api.namespace("perform_query")
 parser = reqparse.RequestParser()
 parser.add_argument("filter", type=str)  # a text to find
 parser.add_argument("limit", type=inputs.positive)  # a number of strings to output
-parser.add_argument("sort", type=str, choices=('asc', 'desc'))  # <asc:desc>
+parser.add_argument("sort", type=str, choices=("asc", "desc"))  # <asc:desc>
 parser.add_argument("map", type=inputs.positive)  # a column number to output
 parser.add_argument("unique", type=str)  # outputs only unique values
 parser.add_argument("filename", type=str, required=True)  # a file to process
@@ -45,14 +45,16 @@ parser.add_argument("filename", type=str, required=True)  # a file to process
 
 
 @index_ns.route("/")
-@index_ns.doc(params={
-    "filter": "Что ищем?",
-    "limit": "Сколько срок выводим?",
-    "sort": "Как сортируем? <asc:desc>",
-    "map": "Какую колонку выводим? (>0)",
-    "unique": "Выводим только уникальные значения",
-    "filename": "Файл, с которым работаем",
-})
+@index_ns.doc(
+    params={
+        "filter": "Что ищем?",
+        "limit": "Сколько срок выводим?",
+        "sort": "Как сортируем? <asc:desc>",
+        "map": "Какую колонку выводим? (>0)",
+        "unique": "Выводим только уникальные значения",
+        "filename": "Файл, с которым работаем",
+    }
+)
 class IndexView(Resource):
     @api.expect(parser)
     def post(self):
@@ -75,28 +77,29 @@ class IndexView(Resource):
             if cmd_unique:
                 if cmd_sort:
                     return sorted(
-                        list(set(find_subsring(source, cmd_filter, cmd_map))), reverse=rev_order
+                        list(set(find_substring(source, cmd_filter, cmd_map))),
+                        reverse=rev_order,
                     )[:cmd_limit]
-                return list(set(find_subsring(source, cmd_filter, cmd_map)))[:cmd_limit]
+                return list(set(find_substring(source, cmd_filter, cmd_map)))[:cmd_limit]
 
             if cmd_sort:
-                return sorted(find_subsring(source, cmd_filter, cmd_map), reverse=rev_order)[:cmd_limit]
-            return find_subsring(source, cmd_filter, cmd_map, cmd_limit)
+                return sorted(
+                    find_substring(source, cmd_filter, cmd_map), reverse=rev_order
+                )[:cmd_limit]
+            return find_substring(source, cmd_filter, cmd_map, cmd_limit)
 
         if cmd_unique:
             if cmd_sort:
-                return sorted(make_unique_lst(source, column=cmd_map), reverse=rev_order)[:cmd_limit]
+                return sorted(
+                    make_unique_lst(source, column=cmd_map), reverse=rev_order
+                )[:cmd_limit]
             return make_unique_lst(source, column=cmd_map, limit=cmd_limit)
 
         if cmd_sort:
-            return sorted(get_strings(source, column=cmd_map), reverse=rev_order)[:cmd_limit]
+            return sorted(get_strings(source, column=cmd_map), reverse=rev_order)[
+                :cmd_limit
+            ]
         return get_strings(source, column=cmd_map, limit=cmd_limit)
-
-        # return f"filter={cmd_filter}, limit={cmd_limit}, sort={cmd_sort}," \
-        #        f" map={cmd_map}, unique={cmd_unique}, filename={filename}"
-
-        # return request.args
-        # return "<h1>Here will be your query results<h1>"
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -15,29 +15,35 @@ def find(file_path, txt):
 
 def read_line_from_file(file_path):
     with open(file_path) as f:
-        # yield (line for line in f)
         while True:
             try:
                 yield next(f)
             except StopIteration:
                 break
-            # if txt in line:
-            #     yield line[:-1]
 
 
-def split_str(string: str) -> list:
-    string_split = re.split('"\s"|"\s|\s"|(\[\])|\s-\s-\s', string)
-    return [item.strip() for item in string_split if item]
+# string_split = re.split('"\s"|"\s|\s"|(\[\])|\s-\s-\s', string)
 
 
-def get_column(string: str, column: int) -> str:
-    string_split = split_str(string)
-    if column > len(string_split):
+def split_str(text: str) -> list:
+    text_split = re.split('"\s"|"\s|\s"|(\[\])|\s-\s-\s', text)  # noqa
+    return [item.strip() for item in text_split if item]
+
+
+def get_column(text: str, column: int) -> str:
+    """
+    returns the value of the column with number column
+    :param text: a string
+    :param column: a column number
+    :return: the value of the column with the specified number
+    """
+    split_text = split_str(text)
+    if column > len(split_text):
         raise IndexError
-    return string_split[column - 1]
+    return split_text[column - 1]
 
 
-def get_strings(source: Generator, column: int = None, limit: int = None):
+def get_strings(source: Generator, column: int = None, limit: int = None) -> list:
     result_lst = []
     for line in source:
         if column:
@@ -54,7 +60,9 @@ def make_unique_lst(source: Generator, column: int = None, limit: int = None) ->
     return list(set([get_column(line, column) for line in source]))[:limit]
 
 
-def find_subsring(source: Generator, substring: str, column: int = None, limit: int = None) -> list:
+def find_substring(
+    source: Generator, substring: str, column: int = None, limit: int = None
+) -> list:
     result_lst = []
     for line in source:
         if column:
@@ -67,15 +75,15 @@ def find_subsring(source: Generator, substring: str, column: int = None, limit: 
 
 
 if __name__ == "__main__":
-    # it = iter(read_line_from_file('./data/apache_logs.txt'))
-    # string = next(it)
-    # print(string)
+    it = iter(read_line_from_file("./data/apache_logs.txt"))
+    string = next(it)
+    print(string)
     # column = get_column(string, 5)
     # print(column)
     #
-    # string_split = split_str(string)
-    # print(string_split)
-    # print(len(string_split))
+    string_split = split_str(string)
+    print(string_split)
+    print(len(string_split))
 
     # print(*(line for line in read_line_from_file('./data/apache_logs.txt') if "17/May" in line))
 
@@ -89,9 +97,8 @@ if __name__ == "__main__":
     # # print(*unique_lst, sep='\n')
     # print(len(unique_lst))
 
-    source = read_line_from_file('./data/apache_logs.txt')
-
-    print(*sorted(make_unique_lst(source, 1))[:10], sep='\n')
+    # source = read_line_from_file("./data/apache_logs.txt")
+    # print(*sorted(make_unique_lst(source, 1))[:10], sep="\n")
 
     # source = read_line_from_file('./data/apache_logs.txt')
-    # print(*find_subsring(source, "20/May/2015:17:05:55", 2), sep='\n')
+    # print(*find_substring(source, "20/May/2015:17:05:55", 2), sep='\n')
